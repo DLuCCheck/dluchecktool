75b02f6272fd3988965cc30fc50bea08d3a27ad4
DLuCCheck <dlucheck@protonmail.com>
2021-03-28 22:50:40 +0300

    Update test

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+import sqlite3
+
 from dlucheck import (
-    FInfo, TInfo, Common,
-    def_condition, find_related_fields)
+    FInfo, TInfo, Common, THandler,
+    def_condition)
 
 
 if __name__ == '__main__':
+    # Read only connection
+    con2 = sqlite3.connect('file:./example.db?mode=ro', uri=True)
+
     t1 = TInfo("example1", [
         FInfo(["id"],                   "int", def_condition),
         FInfo(["name"],                 "str", def_condition),
@@ -22,5 +27,8 @@ if __name__ == '__main__':
         ("date", "str", 0),
     ])
 
-    r1 = (0, 'Victor Fernandez', '2021-01-18 15:10:47')
-    print(find_related_fields(t1, t2, common, r1))
+    handler = THandler(t1, t2, common, None, con2)
+
+    r1 = (1, 'John Wick', '2021-01-18 15:10:47')
+    related_fields = handler.find_related_rows(r1)
+    print(related_fields)
