9ef23174e693dfd8b4118c7e1ea3f26f8117bac2
DLuCCheck <dlucheck@protonmail.com>
2021-04-01 16:32:16 +0300

    Update test

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
 import sqlite3
+import time
 
-from dlucheck import (
-    FieldFormat, TableFormat, Common, THandler,
-    def_condition)
-
-def search_by_words_all(words):
-    # Here {0} stands for the field's name
-    query = ' AND '.join(['{0} LIKE ?' for word in words])
-    return (query, tuple([f'%{word}%' for word in words]))
+# from dlucheck import (
+#     FieldFormat, TableFormat, Common, THandler,
+#     def_condition)
+from dlucheck import THandler, def_condition
+from table import *
+from common_functions import epoch_to_date, search_by_words_all
 
 
 if __name__ == '__main__':
     search_by_names = lambda full_name : search_by_words_all(full_name.split())
-    # Read only connection
-    con2 = sqlite3.connect('file:./example.db?mode=ro', uri=True)
+    # # Read only connection
+    # con2 = sqlite3.connect('file:./example.db?mode=ro', uri=True)
+    con = sqlite3.connect('file:./example.db?mode=ro', uri=True)
 
-    t1 = TableFormat("example1", [
-        FieldFormat(["id"],       "int", def_condition),
-        FieldFormat(["name"],     "str", search_by_names),
-        FieldFormat(["date"],     "str", def_condition),
-    ])
+    t1 = Table("example1", [
+        Field(["id"],       "int", def_condition),
+        Field(["name"],     "str", search_by_names),
+        Field(["date"],     "str", def_condition),
+    ], con)
 
-    t2 = TableFormat("example2", [
-        FieldFormat(["id"],                   "int", def_condition),
-        FieldFormat(["name"],                 "str", search_by_names),
-        FieldFormat(["created_date", "date"], "int", def_condition),
-    ])
+    t2 = Table("example2", [
+        Field(["id"],                   "int", def_condition),
+        Field(["name"],                 "str", search_by_names),
+        Field(["created_date", "date"], "int", def_condition),
+    ], con)
 
     common = Common([
         ("id",   "int", 0.4),
@@ -33,8 +33,22 @@ if __name__ == '__main__':
         ("date", "str", 0.0),
     ])
 
-    handler = THandler(t1, t2, common, None, con2)
+    handler = THandler(t1, t2, common, None, con)
 
     r1 = (1, 'John Wick', '2021-01-18 15:10:47')
     related_fields = handler.find_related_rows(r1)
     print(related_fields)
+
+    # table = Table("example2", [
+    #     Field(["id"],                   "int", def_query_fnc),
+    #     Field(["name"],                 "str", search_by_names),
+    #     Field(["created_date", "date"], "int", def_query_fnc, epoch_to_date),
+    # ], con2)
+
+    # common = Common([
+    #     ("id",   "int", 0.4),
+    #     ("name", "str", 0.6),
+    #     ("date", "str", 0.0),
+    # ])
+
+    # print(table.to_normalized_numpy_array(common))
